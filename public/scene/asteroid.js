eef9d69ebff44786fa74dab8d5d23b51076dec17
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-11-07 23:05:22 -0600

    astroroids look more random andless sharp

--- public/scene/asteroid.js
+++ public/scene/asteroid.js
@@ -3,15 +3,15 @@ import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0?dts";
 
 const noise = new SimplexNoise();
 function createAsteroid(baseRadius = 1) {
-  // Base low-poly icosahedron for randomized asteroid effect
-  const geometry = new THREE.IcosahedronGeometry(baseRadius, 2);
+  // Base sphere randomized asteroid effect
+  const geometry = new THREE.SphereGeometry(baseRadius, 32, 32);
   const pos = geometry.attributes.position;
   const vec = new THREE.Vector3();
 
   // Smooth noise displacement for bumps/craters
   for (let i = 0; i < pos.count; i++) {
     vec.fromBufferAttribute(pos, i);
-    const n = noise.noise3D(vec.x * 3, vec.y * 3, vec.z * 3);
+    const n = noise.noise3D(vec.x * 1, vec.y * 1, vec.z * 1);
     const displacement = n * 0.3; 
     vec.addScaledVector(vec.clone().normalize(), displacement);
     pos.setXYZ(i, vec.x, vec.y, vec.z);
@@ -43,16 +43,17 @@ function createAsteroid(baseRadius = 1) {
     opacity: 0.2,
   });
 
-  const rimSphere = new THREE.Mesh(geometry.clone(), rimMaterial);
-  rimSphere.scale.multiplyScalar(1.05); 
-  asteroid.add(rimSphere);
+    const rimSphere = new THREE.Mesh(geometry.clone(), rimMaterial);
+    rimSphere.scale.multiplyScalar(1.05); 
+    asteroid.add(rimSphere);
 
-  // Random rotation
-  asteroid.rotation.set(
-    Math.random() * Math.PI,
-    Math.random() * Math.PI,
-    Math.random() * Math.PI
-  );
+  // Random scaling
+    const scaleX = (Math.random() * 0.5) + 0.75;
+    const scaleY = (Math.random() * 0.5) + 0.75;
+    const scaleZ = (Math.random() * 0.5) + 0.75;
+
+    asteroid.scale.set(scaleX, scaleY, scaleZ);
+
 
     asteroid.castShadow = true;
     asteroid.receiveShadow = true;

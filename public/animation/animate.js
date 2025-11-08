6650fc101f52aff0dceb15226f3774af94dbaa96
Keehin <ktsmccann@gmail.com>
2025-11-07 23:01:24 -0600

    small tweaks to animation

--- public/animation/animate.js
+++ public/animation/animate.js
@@ -1,14 +1,14 @@
 function animate(meshmap, controls, render, stats) {
   const sphere = meshmap["sphere"];
   const clouds = meshmap["clouds"];
-  const speed = 0.0001;
-  const xspeed = 1;
+  const speed = 0.00001;
+  const xspeed = 6;
   function loop() {
     requestAnimationFrame(loop);
     sphere.rotation.x += xspeed * speed;
-    sphere.rotation.y += 2 * speed;
+    sphere.rotation.y += 20 * speed;
     clouds.rotation.x += xspeed * speed;
-    clouds.rotation.y += 1 * speed;
+    clouds.rotation.y += 10 * speed;
     controls.update();
     render();
     stats.update();
